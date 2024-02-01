214f3f84425e7c8ca0c3ad6066f77ecd03f879b1
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-01 15:32:52 +0100

    secondcommit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn, urlFor } from '../env'
+import { apiVersion, dataset, projectId, useCdn} from '../env'
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn,
-  urlFor,
+  useCdn
+  
 })
