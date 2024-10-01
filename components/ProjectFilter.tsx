07e245e790a70d1d852706a2654b59edb18cc951
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-10-01 16:21:12 +0200

    first commit

--- components/ProjectFilter.tsx
+++ components/ProjectFilter.tsx
@@ -9,6 +9,13 @@ interface Project {
   category?: string;
 }
 
+interface ProjectFilterProps {
+  setFiltered: React.Dispatch<React.SetStateAction<Project[]>>;
+  activeCategory: string;
+  setActiveCategory: React.Dispatch<React.SetStateAction<string>>;
+  projects: Project[];
+}
+
 const ProjectFilter: React.FC<ProjectFilterProps> = ({
   setFiltered,
   activeCategory,
