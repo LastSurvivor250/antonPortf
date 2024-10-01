59c365550d72e6cabd20959a21ee011b82d7c0ef
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-10-01 16:25:15 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,12 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <ProjectFilter />
+      <ProjectFilter
+        setFiltered={setFiltered}
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+        projects={projects}
+      />
       <ProjectCard />
 
       <Skills />
