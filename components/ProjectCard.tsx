cc55c866849dc27a68f3c0b37baf32f84f0e7d78
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-10-02 11:48:25 +0200

    fds

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -42,7 +42,7 @@ const ProjectCard = () => {
       {images.map((image, index) => (
         <div
           key={index}
-          className="shadow-md cursor-pointer relative"
+          className="relative w-[250px] h-[250px] shadow-md cursor-pointer "
           onMouseLeave={() => setHoveredIndex(null)}
           onMouseEnter={() => setHoveredIndex(index)}
         >
@@ -55,20 +55,18 @@ const ProjectCard = () => {
                 role="img"
                 aria-label={`Antonio's Image ${index + 1}`}
                 alt={`Antonio's Image ${index + 1}`}
-                className="md:justify-self-end max-md:w-96"
+                className=" md:justify-self-end max-md:w-96"
               />
             </div>
           </a>
 
           {hoveredIndex === index && ( // Wyświetla tylko dla odpowiedniego obrazka
-            <a href="/#" target="_blank">
-              <div className="bg-purple-500/80 py-1 px-3 absolute w-full h-full top-0 flex justify-center items-center transition-opacity duration-500 opacity-100">
-                <h3 className="text-center text-black text-2xl flex gap-1 items-center font-thin">
-                  Checkout Project
-                  <LiaExternalLinkAltSolid />
-                </h3>
-              </div>
-            </a>
+            <div className="bg-purple-500/80 py-1 px-3 absolute w-full h-full top-0 flex justify-center items-center transition-opacity duration-500 opacity-100">
+              <h3 className="text-center text-black text-2xl flex gap-1 items-center font-thin">
+                Checkout Project
+                <LiaExternalLinkAltSolid />
+              </h3>
+            </div>
           )}
         </div>
       ))}
