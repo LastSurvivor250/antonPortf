c6109e17499b32a70369548e401475e53a1a498d
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-10-02 12:29:17 +0200

    fds

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -8,27 +8,27 @@ const Bio = () => {
           <span className="bg-purple-500 px-2 text-xl font-extrabold border uppercase rounded-md mb-5">
             Bio
           </span>
-          <>
-            <div className="mt-4 text-center">
-              <p className="text-lg font-medium text-gray-800">
-                Who am I? A problem-solver at heart, blending the sharp skills
-                of an anticorrosion engineer with the creative edge of a
-                JavaScript and React developer. But I don’t stop there—I'm also
-                a t-shirt designer with a flair for bold, thought-provoking
-                visuals that get people talking. My mission? To push boundaries,
-                whether I’m coding clean, efficient web applications or creating
-                designs that challenge the status quo. What sets me apart? My
-                diverse skill set. I’m not just an engineer. I’m also a
-                YouTuber, building a channel to spark conversations about ADHD
-                and mental health, inspiring resilience and growth. And did I
-                mention? I'm into combat sports, fitness, and all things manga
-                and anime—fueling my drive to always level up. Ready to
-                innovate? Let’s make something unforgettable—whether it’s
-                building software that works or designs that speak.
-              </p>
-            </div>
-          </>
         </div>
+        <>
+          <div className="mt-4 text-center">
+            <p className="text-lg font-medium text-gray-800">
+              Who am I? <p /> A problem-solver at heart, blending the sharp
+              skills of an anticorrosion engineer with the creative edge of a
+              JavaScript and React developer.
+              <p /> But I don’t stop there—I'm also a t-shirt designer with a
+              flair for bold, thought-provoking visuals that get people talking.
+              My mission? To push boundaries, whether I’m coding clean,
+              efficient web applications or creating designs that challenge the
+              status quo. What sets me apart? My diverse skill set. I’m not just
+              an engineer. I’m also a YouTuber, building a channel to spark
+              conversations about ADHD and mental health, inspiring resilience
+              and growth. And did I mention? I'm into combat sports, fitness,
+              and all things manga and anime—fueling my drive to always level
+              up. Ready to innovate? Let’s make something unforgettable—whether
+              it’s building software that works or designs that speak.
+            </p>
+          </div>
+        </>
       </div>
     </div>
   );
