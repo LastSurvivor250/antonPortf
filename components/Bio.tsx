e5d2f4a2641cf51eb32713d772a3592128c587c9
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-10-02 12:42:13 +0200

    fds

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -12,24 +12,35 @@ const Bio = () => {
         <>
           <div className="mt-4 text-center mb-10">
             <p className="text-lg font-medium text-gray-800">
-              Who am I? <p /> A problem-solver at heart, blending the sharp
-              skills of an anticorrosion engineer with the creative edge of a
-              JavaScript and React developer.
-              <p /> But I don’t stop there <p />
+              <strong>Who am I?</strong> <p />
+              <br /> A problem-solver at heart, blending the sharp skills of an
+              anticorrosion engineer with the creative edge of a JavaScript and
+              React developer.
+              <br />
+              <p /> <strong>But I don’t stop there</strong> <p />
+              <br />
               I'm also a t-shirt designer with a flair for bold,
-              thought-provoking visuals that get people talking. <p />
-              My mission? <p />
+              thought-provoking visuals that get people talking.
+              <br /> <p />
+              <strong>My mission?</strong> <p />
+              <br />
               To push boundaries, whether I’m coding clean, efficient web
               applications or creating designs that challenge the status quo.{" "}
-              <p /> What sets me apart? <p /> My diverse skill set. I’m not just
-              an engineer. I’m also a YouTuber, building a channel to spark
-              conversations about ADHD and mental health, inspiring resilience
-              and growth. <p /> And did I mention? <p />
+              <br />
+              <p />
+              <strong> What sets me apart?</strong> <p />
+              <br /> My diverse skill set. I’m not just an engineer. I’m also a
+              YouTuber, building a channel to spark conversations about ADHD and
+              mental health, inspiring resilience and growth. <br />
+              <p />
+              <strong> And did I mention?</strong> <p />
+              <br />
               I'm into combat sports, fitness, and all things manga and
-              anime—fueling my drive to always level up. <p />
-              Ready to innovate? <p /> Let’s make something
-              unforgettable—whether it’s building software that works or designs
-              that speak.
+              anime—fueling my drive to always level up.
+              <br /> <p />
+              <strong>Ready to innovate?</strong> <p />
+              <br /> Let’s make something unforgettable—whether it’s building
+              software that works or designs that speak.
             </p>
           </div>
         </>
