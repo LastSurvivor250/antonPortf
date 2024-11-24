145910721960060b41efa0c84fb76619ac3087c5
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-11-24 20:57:25 +0100

    yea

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,5 @@
 import ProgressBar from "./ProgressBar";
+import React, { useState } from "react";
 
 const skills = [
   { iconPath: "/html5.png", title: "HTML", percentage: 90 },
@@ -30,20 +31,30 @@ const skills = [
 ];
 
 const Skills = () => {
+  const [isVisible, setIsVisible] = useState(false); // State to track visibility
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible); // Toggle the visibility state
+  };
   return (
     <section id="skills" className="w-full h-fit py-20 relative bg-gray-800">
       <div className="flex justify-center">
-        <span className="bg-purple-500 px-2 text-xl font-extrabold border uppercase rounded-md mb-5">
-          Skills
-        </span>
+        <button
+          onClick={toggleVisibility}
+          className="bg-purple-500 px-2 text-xl font-extrabold border uppercase rounded-md mb-5"
+        >
+          {isVisible ? "Hide Skills" : "Skills"}
+        </button>
       </div>
-      <div className="max-w-[1400px] w-[91%] mx-auto grid md:grid-cols-3 md:gap-10">
-        {skills.map((skill, index) => (
-          <div key={index}>
-            <ProgressBar {...skill} />
-          </div>
-        ))}
-      </div>
+      {isVisible && (
+        <div className="max-w-[1400px] w-[91%] mx-auto grid md:grid-cols-3 md:gap-10">
+          {skills.map((skill, index) => (
+            <div key={index}>
+              <ProgressBar {...skill} />
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
